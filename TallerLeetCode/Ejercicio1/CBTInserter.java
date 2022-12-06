e73b7943cbddc07c8325964e9e0cf9401002db03
Ale <84356480+Alexa-ai17@users.noreply.github.com>
2022-12-06 14:02:41 -0500

    Update CBTInserter.java

--- TallerLeetCode/Ejercicio1/CBTInserter.java
+++ TallerLeetCode/Ejercicio1/CBTInserter.java
@@ -1,5 +1,21 @@
 package Ejercicio1;
 
+
+ Definición de un nodo de árbol binario.
+ public class TreeNode {
+      int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode() {}
+     TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+    }
+  }
+ 
+
 class CBTInserter {
     TreeNode root;
     Deque<TreeNode> deque;
