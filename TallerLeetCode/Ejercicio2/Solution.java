7aa37ece0a17e1e321999ff39c854a0736550ece
Ale <84356480+Alexa-ai17@users.noreply.github.com>
2022-12-06 14:03:57 -0500

    Update Solution.java

--- TallerLeetCode/Ejercicio2/Solution.java
+++ TallerLeetCode/Ejercicio2/Solution.java
@@ -1,6 +1,19 @@
 package Ejercicio2;
 
+* public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
 
+     
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
